703fa26ea3856f81fb65c1605b920e962c721035
Jerry Greenberg <jpg@tscc-3-32.sdsc.edu>
2017-02-10 11:46:46 -0800

    added command in RPM.EXTRAS to prevent "digest mismatch" errors

--- src/intelmpi-rpms/version.mk
+++ src/intelmpi-rpms/version.mk
@@ -1,7 +1,7 @@
 NAME               = sdsc-intelmpi
 VERSION            = 2016.3.210
-RELEASE            = 1
+RELEASE            = 2
 RPM.EXTRAS         = "Autoprov: 0"/nAutoReq:No/n%define  __prelink_undo_cmd %{nil}
 PKGROOT            = /opt/intel/$(VERSION)
 
-RPM.EXTRAS     = AutoReq:No
+RPM.EXTRAS     = AutoReq:No \n %define         __prelink_undo_cmd     %{nil}
