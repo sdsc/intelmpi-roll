e90f75c61a9f54cee15f9ec1a5fc3f4b43c8625f
Jerry Greenberg <jpg@sdsc.edu>
2017-12-04 17:51:33 -0800

    APS-597 intel + mpi 2018

--- src/intelmpi-rpms/version.mk
+++ src/intelmpi-rpms/version.mk
@@ -1,6 +1,6 @@
 NAME               = sdsc-intelmpi
-VERSION            = 2016.3.210
-RELEASE            = 3
+VERSION            = 2018.1.163
+RELEASE            = 0
 RPM.EXTRAS         = "Autoprov: 0"/nAutoReq:No/n%define  __prelink_undo_cmd %{nil}
 PKGROOT            = /opt/intel/$(VERSION)
 
