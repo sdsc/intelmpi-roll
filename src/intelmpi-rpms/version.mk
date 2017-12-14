1cbc2bfaf8591ca441607a69c8e81ce5b0cd853c
Jerry Greenberg <jpg@sdsc.edu>
2017-12-14 11:29:05 -0800

    APS-632 don't run brp-python-bytecompile when building rpm file; also do not assume /opt/intel exists

--- src/intelmpi-rpms/version.mk
+++ src/intelmpi-rpms/version.mk
@@ -1,8 +1,8 @@
 NAME               = sdsc-intelmpi
 VERSION            = 2018.1.163
-RELEASE            = 0
+RELEASE            = 1
 RPM.EXTRAS         = "Autoprov: 0"/nAutoReq:No/n%define  __prelink_undo_cmd %{nil}
 PKGROOT            = /opt/intel/$(VERSION)
 
-RPM.EXTRAS         = AutoReq:No \n %define         __prelink_undo_cmd     %{nil}
+RPM.EXTRAS         = AutoReq:No \n %define         __prelink_undo_cmd     %{nil}\n%define __os_install_post /usr/lib/rpm/brp-python-bytecompile
 RPM.PREFIX         = $(PKGROOT)
