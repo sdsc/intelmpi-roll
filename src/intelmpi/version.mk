42242bbb1e511e7474da6e37b1d4ccfe9bacb39f
Jerry Greenberg <jpg@sdsc.edu>
2018-11-19 13:33:10 -0800

    APS-935 substitute MODULE_UNLOAD_COMPILER for "module unload ..." in intelmpi Makefile

--- src/intelmpi/version.mk
+++ src/intelmpi/version.mk
@@ -3,7 +3,7 @@ ifneq ("$(ROLLOPTS)", "$(subst licenseserver=,,$(ROLLOPTS))")
 endif
 NAME               = sdsc-intelmpi
 VERSION            = 2018.1.163
-RELEASE            = 4
+RELEASE            = 5
 PKGROOT            = /opt/intel/$(VERSION)
 
 SRC_SUBDIR         = intelmpi
